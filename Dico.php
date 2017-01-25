ea7943124bdaf2466842538528d62b08f2aca205
Dylan <dylan4047@gmail.com>
2017-01-25 12:32:50 +0100

    suppresion premiere boucle et affichage du nombre de mots faisant 15 charachtere

--- Dico.php
+++ Dico.php
@@ -2,7 +2,14 @@
 $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
 $total = count($dico);
- ?>
+$aray=[];
+foreach ($dico as $key => $value) {
+	$countArray = count($aray);
+	if (strlen($dico[$key]) === 15) {
+		array_push($aray, $dico[$key]);
+	}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,19 +17,20 @@ $total = count($dico);
 	<title>Dico</title>
 </head>
 <body>
-<div>
-	<h3>Combien de Mots contient ce dictionaire ?</h3>
 	<div>
-		
-		<?php foreach($dico as $key => $value): ?>
-		<span>
-			ce Dictionaire contient : <?= $total?> Mots
-		</span>
-		<?php die(); ?>
-		<?php endforeach?>
-
-
+		<h3>Combien de Mots contient ce dictionaire ?</h3>
+		<div>
+			<span>
+				ce Dictionaire contient : <?= $total?> Mots
+			</span>
+		</div>
 	</div>
-</div>
+	<div>
+		<h3>Combien de mots font exactement 15 caractères ?</h3>
+		<span>
+			Le nombre de mots faisant 15 caractères est :<?= $countArray?>
+		</span>
+	</div>
 </body>
 </html>
+

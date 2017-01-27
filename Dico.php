d674cd9eab4fcb36793f61c600237c76f2172ac1
Dylan <dylan4047@gmail.com>
2017-01-27 05:03:35 +0100

    affichage du nombre de mot terminant par la lettre "q"

--- Dico.php
+++ Dico.php
@@ -4,15 +4,20 @@ $dico = explode("\n", $string);
 $total = count($dico);
 $aray=[];
 $arryContainW=[];
+$arrayEnd=[];
 foreach ($dico as $key => $value) {
 	$countArray = count($aray);
 	$countArrayContainW = count($arryContainW);
+	$countArrayEnd = count($arrayEnd);
 	if (strlen($dico[$key]) === 15) {
 		array_push($aray, $dico[$key]);
 	}
 	if (strpos($dico[$key], "w")) {
 		array_push($arryContainW , $dico[$key]);
 		
+	}
+	if (substr($dico[$key], -1) === "q") {
+		array_push($arrayEnd, $dico[$key]);
 	}
 }
 ?>
@@ -41,6 +46,10 @@ foreach ($dico as $key => $value) {
 		<h3>Combien de mots contiennent la lettre « w » ?</h3>
 		<span><?= $countArrayContainW?> Mots contiennent la letre "w"</span>
 
+	</div>
+	<div>
+		<h3>Combien de mots finissent par la lettre « q » ?</h3>
+		<span><?= $countArrayEnd?> Mots finissent par "q"</span>
 	</div>
 </body>
 </html>

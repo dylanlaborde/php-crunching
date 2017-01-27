f4c9dd4805411080d3c6f3638d855c6bce7de2e8
Dylan <dylan4047@gmail.com>
2017-01-27 05:55:00 +0100

    affichage du classement du fim gravity

--- List_Film.php
+++ List_Film.php
@@ -3,6 +3,13 @@
 $string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
 $brut = json_decode($string, true);
 $top = $brut["feed"]["entry"]; # liste de films
+
+foreach ($top as $key => $value) {
+	// Classement "Gravity"
+	if ($value["im:name"]["label"] === "Gravity") {
+		$classement = array_search($value, $top);
+	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,15 +18,27 @@ $top = $brut["feed"]["entry"]; # liste de films
 	<title>Liste des Film</title>
 </head>
 <body>
+<!-- exercice 1 -->
 	<div>
 		<h3>Top 10 des Film</h3>
 		<ul>
-			<?php for ($i=0; $i < 9 ; $i++) { ?>
+			<?php for ($i=0; $i < 10 ; $i++) { ?>
 
 			<li><?= $i+1 .":"."\n" ?><?=  $top[$i]["im:name"]["label"];?> </li>
 
 			<?php } ?>
 		</ul>
+	</div>
+	<!-- exercice 2 -->
+	<div>
+		<h3>
+			Classement du Film Gravity :
+		</h3>
+		<span>le film "Gravity" est a la position <?= $classement+1 ."."?></span>
+	</div>
+<!-- exercice 3 -->
+	<div>
+		<h3>Quel est le réalisateur du film « The LEGO Movie » ?</h3>
 	</div>
 </body>
 </html>
